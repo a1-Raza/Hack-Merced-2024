6f7747e602addd3539dac2242078eb02796085c6
John Bouyer <jbouyer151@gmail.com>
2024-03-09 10:26:37 -0800

    updated satellite api

--- satellite-api.py
+++ satellite-api.py
@@ -7,7 +7,7 @@ dotenv.load_dotenv()
 NASA_API_KEY="PoptCcoOSqAe4porNulk0WfWuGqkd8afZWRtGmKq"
 lat=str(37.38)
 lon=str(-120.43)
-date="2023-01-01"
+date="2021-01-01"
 dim=str(0.10)
 #37.366389, -120.424167
 
@@ -18,7 +18,18 @@ if response.status_code == 200:
     try:
         # Try to parse the response as JSON
         data = response.json()
-        print(data)
+        image_url = data['url']
+
+        # Now make a request to the image URL to check the Content-Type header
+        image_response = requests.get(image_url)
+
+        if image_response.status_code == 200:
+            # Check the Content-Type header to determine the image format
+            content_type = image_response.headers.get("Content-Type", "")
+            print(f"Image Format: {content_type}")
+        else:
+            print(f"Error {image_response.status_code}: {image_response.text}")
+
     except requests.exceptions.JSONDecodeError:
         print("Error: Response content is not in JSON format")
 else:

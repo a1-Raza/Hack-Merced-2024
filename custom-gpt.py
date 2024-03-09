d89f5311090b51d6c5ed413e8279ba8c2c83abee
John Bouyer <jbouyer151@gmail.com>
2024-03-08 22:50:09 -0800

    Added more specific helper functions

--- custom-gpt.py
+++ custom-gpt.py
@@ -7,12 +7,40 @@ from openai import OpenAI
 
 # todo add function that returns messages from gpt
 
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
+
+
 def compare_images_from_url_with_message(url1: str, url2: str, user_input: str = None):
     return [
         {
             "role": "user",
             "content": [
-                {"type": "text", "text":  # user_input
+                {"type": "text", "text": user_input},
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": url1,
+                    },
+                },
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": url2
+                    },
+                },
+            ],
+        }
+    ]
+
+
+def compare_images_from_url_without_message(url1: str, url2: str):
+    return [
+        {
+            "role": "user",
+            "content": [
+                {"type": "text", "text":
                     "What’s the difference between these 2 images of the Aral Sea? Is the "
                     "lake more full in the first or second image?"},
                 {
@@ -42,7 +70,7 @@ def new_gpt():
             break
         completion = client.chat.completions.create(
             model="gpt-4-vision-preview",
-            messages=compare_images_from_url_with_message("https://upload.wikimedia.org/wikipedia/"
+            messages=compare_images_from_url_without_message("https://upload.wikimedia.org/wikipedia/"
                                                           "commons/a/a5/Aralsea_tmo_2014231_lrg.jpg",
                                                           "https://upload.wikimedia.org/wikipedia/"
                                                           "commons/2/23/Aral_Sea_August_2017.jpg"
